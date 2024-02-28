13189941e3d3012804e0e6ba1c27f03539482de2
Muthukrishnan-2710 <152172317+Muthukrishnan-2710@users.noreply.github.com>
2024-02-28 10:02:29 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,12 +1,13 @@
+const validUsername = "admin";
+const validPassword = "Welcome123";
+            
+
 document.getElementById("login-form").addEventListener("submit", function(event) {
  event.preventDefault();
 
 var username = document.getElementById("username").value;
 var password = document.getElementById("password").value;
 
-const validUsername = "admin";
-const validPassword = "Welcome123";
-            
 if (username === validUsername && password === validPassword) {
     alert("Login successful!"); 
 } else {
